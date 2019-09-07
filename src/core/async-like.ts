c346f83800ac8eb3bb42956fe1ea9cb08e31ca07
Fabian Cook <hello@fabiancook.dev>
2019-09-07 14:43:32 +1200

    Add additional iterator functions

--- src/core/async-like.ts
+++ src/core/async-like.ts
@@ -39,3 +39,55 @@ export function isIterable<T>(value: unknown): value is Iterable<T> {
     value[Symbol.iterator] instanceof Function
   );
 }
+
+export function isIterableIterator(value: unknown): value is (IterableIterator<any> | AsyncIterableIterator<any>)  {
+  function isIteratorLike(value: unknown): value is { next?: unknown } {
+    return typeof value === "object";
+  }
+  return (
+    isIteratorLike(value) &&
+    value.next instanceof Function &&
+    (
+      isAsyncIterable(value) ||
+      isIterable(value)
+    )
+  );
+}
+
+export function isPromise<T = unknown>(value: unknown): value is Promise<T> {
+  function isPromiseLike(value: unknown): value is { then?: unknown } {
+    return typeof value === "object";
+  }
+  return (
+    isPromiseLike(value) &&
+    value.then instanceof Function
+  );
+}
+
+export async function getNext<T>(iterator: Iterator<T> | AsyncIterator<T>, value?: any): Promise<IteratorResult<T>> {
+  let next: IteratorResult<T> | Promise<IteratorResult<T>>;
+  try {
+    next = iterator.next(value);
+    if (isPromise(next)) {
+      next = await next;
+    }
+    if (next.done) {
+      if (iterator.return) {
+        next = iterator.return(value);
+        if (isPromise(next)) {
+          next = await next;
+        }
+      }
+    }
+    return next;
+  } catch (e) {
+    if (!iterator.throw) {
+      throw e;
+    }
+    next = iterator.throw(e);
+    if (isPromise(next)) {
+      next = await next;
+    }
+    return next;
+  }
+}
